80e15aa939a5d6278b37b4d7145aadace56df4cc
iqbalhossain <iqbalhossainsha2303@gmail.com>
2021-08-22 02:43:49 +0600

    authentication system

--- src/App.js
+++ src/App.js
@@ -1,23 +1,99 @@
-import logo from './logo.svg';
+/* eslint-disable no-undef */
 import './App.css';
+import firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from './firebase_config';
+import { useState } from 'react';
+
+
+// firebase initialization
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}else {
+  firebase.app(); // if already initialized, use that one
+}
 
 function App() {
+  const [user, setUser] = useState({
+    isSignIn: false,
+    name: '',
+    email: '',
+    password: '',
+    photo: '',
+    success: false,
+    error:'',
+  });
+
+  // handle Change
+  const handleChange = (event) => {
+    // console.log(event.target.name, event.target.value);
+    let isFieldValid = true;
+    if (event.target.name == 'email') {
+      isFieldValid = /\S+@\S+\.\S+/.test(event.target.value);
+
+    }
+    if (event.target.name == 'password') {
+      const isPasswordValid = event.target.value.length > 8;
+      const isPasswordHasNumber = /\d{1}/.test(event.target.value);
+      isFieldValid = (isPasswordValid && isPasswordHasNumber);
+
+    }
+    if (isFieldValid) {
+      const newUserInfo = { ...user };
+      newUserInfo[event.target.name] = event.target.value;
+      setUser(newUserInfo);
+
+    }
+
+  }
+
+
+  // Form submit 
+  const handleSubmit = (event) => {
+    // console.log(user.email,user.password);
+    if (user.email && user.password) {
+      firebase.auth().createUserWithEmailAndPassword(user.email, user.password)
+        .then((res) => {
+          console.log(res);
+          const newUserInfo = {...user};
+          newUserInfo.success = true;
+          newUserInfo.error = '';
+          setUser(newUserInfo);
+        })
+        .catch((error) => {
+          // const errorCode = error.code;
+          // const errorMessage = error.message;
+          // console.log(errorMessage);
+          const newUserInfo = { ...user };
+          newUserInfo.success = false;
+          newUserInfo.error = error.message;
+          setUser(newUserInfo);
+        });
+    }
+
+    event.preventDefault();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="my-5">Our own Authentication</h1>
+      {/* <h3>Name: {user.name}</h3>
+      <p>Email: {user.email}</p>
+      <p>Password: {user.password}</p> */}
+      <form onSubmit={handleSubmit} >
+        <input onBlur={handleChange} className="form-control text-field" type="text" name="name" id="" placeholder="Name" required />
+        <br />
+        <input onBlur={handleChange} className="form-control text-field" type="email" name="email" placeholder="Your Email" required />
+        <br />
+        <input onBlur={handleChange} className="form-control text-field" type="password" name="password" placeholder="Password" required />
+        <br />
+        <input type="submit" value="Submit" className="btn btn-primary"/>
+      </form>
+      {/* Error Message */}
+      <p style={{color: 'red',textAlign: 'center',marginTop: '50px'}}>{user.error}</p>
+      {
+        user.success && <p style={{color:'green',textAlign: 'center'}}>User created successfully</p>
+      }
     </div>
   );
 }
---
+++ src/firebase_config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDem3IKOj-N0I0UGUy_i8KfNCwqxm3c1Jg",
+    authDomain: "simple-react-authentication.firebaseapp.com",
+    projectId: "simple-react-authentication",
+    storageBucket: "simple-react-authentication.appspot.com",
+    messagingSenderId: "264692012759",
+    appId: "1:264692012759:web:e4e9c853177a556e628292"
+};
+
+export default firebaseConfig;
